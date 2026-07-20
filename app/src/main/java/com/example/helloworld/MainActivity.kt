4720a85f9da2511a06634c0d104bf00755a9aa8e
Megha_Chavan <chavanmegha665@gmail.com>
2026-07-20 13:35:27 +0530

    Changes to Hello World Project

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.helloworld
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val btnClick = findViewById<Button>(R.id.btnClick)
+        btnClick.setOnClickListener {
+            Toast.makeText(this, "Moved to next page",Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, androidPageActivity::class.java))
         }
     }
 }
